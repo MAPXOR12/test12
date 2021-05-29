4cebdeae07f9fb0d1a559783f58d5d845945c7cc
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-05-29 13:41:32 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,31 +1,83 @@
-const { MessageEmbed } = require("discord.js");
-const { LOCALE } = require("../util/EvobotUtil");
-const i18n = require("i18n");
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
 
-i18n.setLocale(LOCALE);
+  
+
 
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: i18n.__("help.description"),
+  cooldown: 8,
+  description: "**all commands Pinky**",
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-      .setTitle(i18n.__mf("help.embedTitle", { botname: message.client.user.username }))
-      .setDescription(i18n.__("help.embedDescription"))
-      .setColor("#F8AA2A");
+    .setThumbnail(``)
+    .setImage(``)
+    .setTitle(`Pinkt Is One`)
+    .setDescription(`
 
-    commands.forEach((cmd) => {
-      helpEmbed.addField(
-        `**${message.client.prefix}${cmd.name} ${cmd.aliases ? `(${cmd.aliases})` : ""}**`,
-        `${cmd.description}`,
-        true
-      );
-    });
 
-    helpEmbed.setTimestamp();
+ [ Link Bot ]() · [ Server Support ]()
 
+List of all commands
+• ────── emoji_102 ────── •
+☢◢User Commands
++invite - +ping 
++stats - +support
++hosting - +docs
++howtohost - +djs
+
+• ────── RAES ────── •
+☢◢Music Commands
++addprevious - +addsimilar 
++autoplay - +join
++jump - +loob 
++loobqueue - +lyrics
++move - +moveme 
++nowplaying - +pause
++play(p) - +playlist
++playsc - +play skip 
++queue - +radio
++search - +shuffle 
++stop - +volume 
+────── Sixo ────── •
++8ball - +delete
++fact - +joke
++kiss - +meme
++rip - +shit
++say - +purge
+• ────── RAES ────── •
++addbotchat - +adddj
++prefix - +removebotchat 
++reset - settings - +setup 
++toggledjoinly - +toggleplaymssnge
+
+• ────── emoji_97 ────── •
+☢◢Moderator Commands
++avatar - +botinfo - +help
++invite - +ping - +reactions
++serverinfo - +stats - +userinfo 
++uptime - +ban - +kick 
++embed - +slawmode
++unmute - +mute
++unwarn - +warn - +warnings 
+
+• ────── FILTER────── •
+☢◢Owner Commands
++bassboost - +cleareq 
++clearfilter - +equalizer 
+• ────── emoji_107 ────── •
+☢◢Links
+`)
+
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("RANDOM")
+   message.react("✅")
     return message.channel.send(helpEmbed).catch(console.error);
+
   }
 };
