cec748e0547af153d11b98941eb342fb4e720eab
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-03 18:06:27 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -20,58 +20,31 @@ module.exports = {
     .setTitle(`Pinkt Is One`)
     .setDescription(`
 
-
- [ Link Bot ]() · [ Server Support ]()
-
 List of all commands
 • ────── emoji_102 ────── •
 ☢◢User Commands
-+invite - +ping 
-+stats - +support
-+hosting - +docs
-+howtohost - +djs
+${PREFIX invite - ${PREFIX ping
 
 • ────── RAES ────── •
 ☢◢Music Commands
-+addprevious - +addsimilar 
-+autoplay - +join
-+jump - +loob 
-+loobqueue - +lyrics
-+move - +moveme 
-+nowplaying - +pause
-+play(p) - +playlist
-+playsc - +play skip 
-+queue - +radio
-+search - +shuffle 
-+stop - +volume 
-────── Sixo ────── •
-+8ball - +delete
-+fact - +joke
-+kiss - +meme
-+rip - +shit
-+say - +purge
-• ────── RAES ────── •
-+addbotchat - +adddj
-+prefix - +removebotchat 
-+reset - settings - +setup 
-+toggledjoinly - +toggleplaymssnge
+
+ lyrics
+${PREFIX} move - ${PREFIX} moveme 
+${PREFIX} nowplaying - ${PREFIX}play(p)
+${PREFIX} lqueue - ${PREFIX} liyars
+${PREFIX} search - ${PREFIX} shuffle 
+${PREFIX} stop -${PREFIX} volume 
+────── ────── •
+
+☢◢GIF Commands
+${PREFIX}boy - ${PREFIX}gifblack
 
 • ────── emoji_97 ────── •
 ☢◢Moderator Commands
-+avatar - +botinfo - +help
-+invite - +ping - +reactions
-+serverinfo - +stats - +userinfo 
-+uptime - +ban - +kick 
-+embed - +slawmode
-+unmute - +mute
-+unwarn - +warn - +warnings 
-
-• ────── FILTER────── •
-☢◢Owner Commands
-+bassboost - +cleareq 
-+clearfilter - +equalizer 
-• ────── emoji_107 ────── •
-☢◢Links
+${PREFIX}help - ${PREFIX}ping
+${PREFIX}ban - ${PREFIX}unban
+${PREFIX}lock - ${PREFIX}unlock
+${PREFIX}giveway - ${PREFIX}invites
 `)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
